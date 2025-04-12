1488e62b1162f76abb4272eebf5d376eb94ad429
Jayson Martinez <jlmartinez631@gmail.com>
2025-04-12 13:45:17 +0800

    Commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,11 @@
+import { scan } from "react-scan";
+
 import React from "react";
 import ReactDOM from "react-dom/client";
+
 import App from "./App";
+import { themeConfig } from "ui/theme";
+import { TamaguiProvider } from "tamagui";
 
 import "swiper/css";
 import "swiper/css/pagination";
@@ -12,7 +17,20 @@ import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
 
 const rootEl = document.getElementById("root");
+
+const isEnableReactScan = false;
+
+if (isEnableReactScan) {
+  scan({
+    enabled: true,
+  });
+}
+
 if (rootEl) {
   const root = ReactDOM.createRoot(rootEl);
-  root.render(<App />);
+  root.render(
+    <TamaguiProvider config={themeConfig}>
+      <App />
+    </TamaguiProvider>
+  );
 }
